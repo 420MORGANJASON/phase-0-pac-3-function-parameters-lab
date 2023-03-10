3c8e2eea3b657f5af7a6b872a9c3e44f993e2b77
MORGANJASON <420morganjason@gmail.com>
2023-03-10 07:45:58 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+function introduction(name){
+    return (`Hi, my name is ${name}.`);
+}console.log(introduction("Morgan"))
+
+ 
+
+// "Hi, my name is Morgan."
+
+//second test
+
+ function introductionwithLanguage(name, language){
+
+    
+    return `Hi, my name is $(name) and I am learning to program in $(language)`;
+
+} 
+    
+
+
+
+//third test
+function introductionWithLanguageOptional(name, language="JavaScript"){
+
+return `Hi, my name is $(Morgan) and I am learning to program in $(language).`;
+
+}
+
+//4th test
+function sayHelloTo(firstName = "User") {
+   
+  }console.log(sayHelloTo (Hello, ${"firstName"}!));
+    
+    
