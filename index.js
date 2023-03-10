fa99fb5e93142c5369d3f762a6b30f5809ca5760
MORGANJASON <420morganjason@gmail.com>
2023-03-10 15:13:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
- function introduction(name)
-{
+function introduction(name){
     return `Hi, my name is ${name}.`;
 }
-//second test
+
 function introductionWithLanguage(name, language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-// third test
+
 function introductionWithLanguageOptional(name, language= "JavaScript"){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-// 4th test
+
 function introductionWithLanguageOptional(name, language="JavaScript"){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
